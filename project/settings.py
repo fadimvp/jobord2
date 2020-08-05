a59ba534464bf0dd3583f6c388e064d46e05c04a
fadimvp1 <fadimvp@gmail.com>
2020-08-05 16:33:51 +0300

    all hosting ip

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '_ok(aspq07d(72pfb9rn^xsqc(&s#d(w&r=gn$qmo!3l)y@h3c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['obord2.herokuapp.com/','0.0.0.0:5000/']
+ALLOWED_HOSTS = ['https://jobord2.herokuapp.com/','0.0.0.0:5000']
 
 # Application definition
 
