b306955938807f13c256d4606e667093a1b298fe
fadimvp1 <fadimvp@gmail.com>
2020-08-05 23:20:01 +0300

    add requirements

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts import views
 app_name = 'accounts'
 urlpatterns = [
     path('signup', views.Siginup, name='signup'),
-    path('profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('profile_edit', views.profile_edit, name='profile_edit'),
 
 ]
